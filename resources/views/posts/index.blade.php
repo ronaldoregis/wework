b6806bc55b7f3a68e97e964e54aaa9b43d1f214f
Ronaldo Regis <ronaldojprf@gmail.com>
2017-09-04 14:56:19 +1000

    index.blade fix

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,18 +3,15 @@
 @section('content')
     <h1 class="text-center">Posts</h1>
 
-    {{--@if(Auth::check())--}}
-        {{--<div class="text-center">--}}
-            {{--{{url()->}}--}}
-        {{--</div>--}}
-    {{--@endif--}}
-
     @if($posts)
         <div class="text-center">
-        @foreach($posts as $post)
-        <h3>{{$post->title}}</h3><br>
-        {{$post->body}}<br><hr>
-        @endforeach
+            @foreach($posts as $post)
+                <h3>{{$post->title}}</h3><br>
+                {{$post->body}}<br>
+                <div class="image-container">
+                    <img height="100" src="images/{{$post->path}}" alt="">
+                </div>
+            @endforeach
         </div>
     @endif
 @stop
